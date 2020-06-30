287d3548738151d5c139319c9f6507f27f0425c9
DominikPeacock <der.dominik.peacock@gmail.com>
2020-06-30 10:11:22 +0200

    Created project files

---
+++ Source/UnrealChat/UnrealChat.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealChat.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealChat, "UnrealChat" );
